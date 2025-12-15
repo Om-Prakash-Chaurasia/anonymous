cbaa6b83709adf3c5742c493ab0ffbe5e6433e4a
Om-Prakash-Chaurasia <omchaurasia2024@gmail.com>
2025-12-15 13:50:17 +0530

    updated cors at backend

--- server.js
+++ server.js
@@ -2,12 +2,15 @@ const express = require("express"); // importing express
 const dotenv = require("dotenv"); // importing dotenv
 const connectDB = require("./config/db"); // importing db connection
 const userRoutes = require("./routes/user.routes");
+const cors = require("cors");
 
 const app = express(); // creating app
 
 dotenv.config(); // configuring dotenv
 
 app.use(express.json()); //  middleware to convert into json
+
+app.use(cors());
 
 // app.use("/api/user", require("./routes/user.routes"));
 app.use("/api/user", userRoutes);
